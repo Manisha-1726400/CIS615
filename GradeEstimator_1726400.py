d6aea590013721f262b28822190e6e9349076faf
Manisha-1726400 <1726400@park.edu>
2025-03-07 16:50:21 -0600

    Add files via upload

--- GradeEstimator_1726400.py
+++ GradeEstimator_1726400.py
@@ -1,14 +1,3 @@
-
-# Prompt the user to enter their first name and storing the input in a variable.
-first_name = input("Enter your first name: ") 
-# Prompt the user to enter their last name and storing the input in a another different variable.
-last_name = input("Enter your last name: ")
-# Display's the complete name by joining the first name and last name together. 
-print(first_name,last_name)
-
-##unit 8 Assignemnt
-
-
 #Importing pandas and json libraries
 import pandas as pd
 import json
